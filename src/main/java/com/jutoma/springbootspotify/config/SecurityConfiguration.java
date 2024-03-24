ab18f32116495665070f5825b3c1a068da7a78fe
JustinKSU <JustinKSU@gmail.com>
2024-03-24 18:28:18 -0500

    WIP

--- src/main/java/com/jutoma/springbootspotify/config/SecurityConfiguration.java
+++ src/main/java/com/jutoma/springbootspotify/config/SecurityConfiguration.java
@@ -1,19 +1,21 @@
 package com.jutoma.springbootspotify.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .oauth2Login();
+public class SecurityConfiguration {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+                .oauth2Login(withDefaults());
+        return http.build();
     }
 }
